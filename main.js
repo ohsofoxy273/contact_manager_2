be45246719f1d3f9ac6e15f3db068705b58c2f3d
ohsofoxy273 <matthew.f.fox@gmail.com>
2017-11-07 12:54:11 -0600

    make tags and contacts separate objects

--- main.js
+++ main.js
@@ -1,10 +1,96 @@
+function Contact(id, name, phone, email, tags=[]) {
+  this.id    = id;
+  this.name  = name;
+  this.phone = phone;
+  this.email = email;
+  this.tags  = tags;
+}
+
+Contact.prototype = {
+  getNextID: function() {
+    let lastId = 0;
+    if (Contacts.anyContacts()) {
+      let lastId = Contacts.list.slice(-1)[0]['id'];
+    }
+    return lastId;
+  }, 
+};
+
+let Contacts = {
+  list: JSON.parse(localStorage.getItem('contacts')) || [],
+
+  anyContacts: function() {
+    return this.list.length > 0;
+  },
+
+  add: function(contactObject) {
+    if (this.anyContacts()) {
+      let contactsArray = this.list;
+      let lastId = contactsArray.slice(-1)[0]['id'];
+      contactObject.id = Number(lastId) + 1;
+      contactsArray.push(contactObject);
+      localStorage.setItem('contacts', JSON.stringify(contactsArray));
+    } else {
+      contactObject.id = 0;
+      localStorage.setItem('contacts', JSON.stringify([contactObject]));
+    }
+    this.list = JSON.parse(localStorage.getItem('contacts'));
+  },
+
+  edit: function(contactObject) {
+    let id = contactObject.id;
+    let contactsArray = this.list;
+    for (let i = 0; i < contactsArray.length; i++) {
+      if (Number(contactsArray[i]['id']) === Number(id)) {
+        contactsArray[i] = contactObject;
+        break;
+      }
+    }
+    localStorage.setItem('contacts', JSON.stringify(contactsArray));
+  },
+
+  delete: function(id) {
+    let contactsArray = this.list;
+    for (let i = 0; i < contactsArray.length; i++) {
+      if (Number(contactsArray[i]['id']) === Number(id)) {
+        contactsArray.splice(i, 1);
+        break;
+      }
+    }
+    localStorage.setItem('contacts', JSON.stringify(contactsArray));
+  },
+
+  anyActiveTags: function() {
+
+  }
+}
+
+function Tag(value) {
+  this.value = value;
+}
+
+let Tags = {
+  list: [],
+  active: [],
+
+  add: function(tag) {
+    this.list.push(tag);
+  },
+
+  addActive: function(tag) {
+    this.active.push(tag);
+  },
+
+  removeActive: function(tag) {
+    let idx = this.active.indexOf(tag);
+    this.active.splice(idx, 1);
+  },
+
+}
+
 let App = {
   $createContactForm: $('#create_contact'),
   $editContactForm:   $('#edit_contact'),
-
-  retrieveContacts: function() {
-    return JSON.parse(localStorage.getItem('contacts'));
-  },
   
   compile: function(contact) {
     $templates = $('script[type="text/x-handlebars"]');
@@ -14,7 +100,7 @@ let App = {
       let id = $(template).attr('id').replace('_template', '');
       $(`#${id}`).empty();
       if (id === 'contacts') {
-        $(`#${id}`).append(compiledTemplate({contacts: (contact || this.retrieveContacts())}));
+        $(`#${id}`).append(compiledTemplate({contacts: (contact || Contacts.list)}));
       } else if (id === 'edit_contact') {
         $(`#${id}`).append(compiledTemplate(contact));
       } else {
@@ -33,18 +119,13 @@ let App = {
 
   displayContacts: function() {
     $('#contacts').slideDown('slow');
-    if (this.anyContacts()) {
+    if (Contacts.anyContacts()) {
       $('#no_contacts').hide();
       $('#list_contacts').show();
     } else {
       $('#no_contacts').show();
       $('#list_contacts').hide();
     }
-  },
-
-  anyContacts: function() {
-    if (localStorage.getItem('contacts') === null) {return false}
-    return !(localStorage.getItem('contacts') === '[]');
   },
 
   showForm: function(form) {
@@ -58,44 +139,9 @@ let App = {
     return $(this).closest('div').attr('id').match(regexp);
   },
 
-  addContact: function(contactObject) {
-    if (this.anyContacts()) {
-      let contactsArray = JSON.parse(localStorage.getItem('contacts'));
-      let lastId = contactsArray.slice(-1)[0]['id'];
-      contactObject.id = Number(lastId) + 1;
-      contactsArray.push(contactObject);
-      localStorage.setItem('contacts', JSON.stringify(contactsArray));
-    } else {
-      contactObject.id = 0;
-      localStorage.setItem('contacts', JSON.stringify([contactObject]));
-    }
-  },
-
-  editContact: function(contactObject) {
-    let id = contactObject.id;
-    let contactsArray = this.retrieveContacts();
-    for (let i = 0; i < contactsArray.length; i++) {
-      if (Number(contactsArray[i]['id']) === Number(id)) {
-        contactsArray[i] = contactObject;
-        break;
-      }
-    }
-    localStorage.setItem('contacts', JSON.stringify(contactsArray));
-  },
-
-  deleteContact: function(id) {
-    let contactsArray = this.retrieveContacts();
-    for (let i = 0; i < contactsArray.length; i++) {
-      if (Number(contactsArray[i]['id']) === Number(id)) {
-        contactsArray.splice(i, 1);
-        break;
-      }
-    }
-    localStorage.setItem('contacts', JSON.stringify(contactsArray));
-  },
-
   bindButtons: function() {
     let self = this;
+
     $('.add').click(e => self.showForm(self.$createContactForm));
 
     $('.edit').click( e => {
@@ -121,7 +167,7 @@ let App = {
       if (confirm('Are you sure you want to delete this contact?')) {
         contactSection = $(e.target).closest('section');
         id             = contactSection.data('id');
-        self.deleteContact(id);
+        Contacts.delete(id);
         self.compile();
         self.displayContacts();
       }
@@ -136,11 +182,22 @@ let App = {
 
     $('.submit').click(function(e) {
       e.preventDefault();
-      let formParams = self.submitForm.call(this);
+      let formParams = self.submitForm.call(this, self);
       if (formParams && self.isFormType.call(this, 'create')) {
-        self.addContact(formParams);
+        let id;
+        contactTags = [];
+        let contact = new Contact(id, formParams.name, formParams.phone, formParams.email, contactTags);
+        formParams.tags.forEach(tag => {
+          contact.tags.push(tag);
+          if (!Tags.list.some(tags => tags === tag)) {
+            Tags.list.push(tag);
+          }
+        });
+        
+        contact.id =  contact.getNextID();
+        Contacts.add(contact);
       } else if (formParams && self.isFormType.call(this, 'edit')) {
-        self.editContact(formParams)
+        Contacts.edit(formParams);
       }
       $(this).closest('form')[0].reset();
       self.compile();
@@ -152,52 +209,68 @@ let App = {
     });
 
     $('dt.tag').click(function(e) {
-      let tagText = $(e.target).text();
-      contacts = self.selectContactsByTag(tagText);
-      self.compile(contacts);
+      let active = $(this).hasClass('active');
+      let notActive = !active;
+      let text = $(this).text();
+      if (notActive) {
+        Tags.addActive(text);
+      } else {
+        Tags.removeActive(text);
+      }
+      self.displayActiveClass();
+      self.addActiveClass();
     });
   },
 
-  selectContactsByTag: function(tagText) {
-    let contactsArray = this.retrieveContacts();
-    let filteredArray = [];
-    let regexp = new RegExp(tagText, 'i');
-    for (let i = 0; i < contactsArray.length; i++) {
-      let tags = contactsArray[i].tags;
-      if (tags.some(tag => tag.match(regexp))) {
-        filteredArray.push(contactsArray[i])
-      } 
-    }
-    return filteredArray;
+  addActiveClass() {
+    $('dt.tag').each((idx, tag) => {
+      if (Tags.active.includes($(tag).text())) {
+        $(tag).addClass('active');
+      }
+    });
   },
 
-  submitForm: function() {
+  displayActiveClass: function() {
+    let contacts = [];
+    
+    Contacts.list.forEach(contact => {
+      let contactHasAllTheActiveTags = Tags.active.every( r => contact.tags.indexOf(r) >= 0);
+      if (contactHasAllTheActiveTags) {
+        contacts.push(contact);
+      }
+    });
+
+    this.compile(contacts);
+    this.displayContacts();
+  },
+
+  submitForm: function(self) {
     let form = $(this).closest('form');
-    let [name, email, phone, tags] = [formVal("name"), formVal("email"), formVal("phone"), tagValues()];
+    let [name, email, phone, tags] = [self.formVal("name", form), self.formVal("email", form), self.formVal("phone", form), self.tagValues(form)];
     let id;
 
     if (form.find('input[type="hidden"]').length > 0) {
       id = form.find('input[type="hidden"]').val();
     }
 
-    function formVal(string) {
-      return form.find(`input[name="${string}"]`).val();
-    }
-
-    function tagValues() {
-      let tagVals = form.find(`input[name="tags"]`).val();
-      let tags = tagVals.split(',').map(string => string.trim());
-      if (tags.length === 0 || (tags.length === 1 && tags[0] === '')) {
-        return [];
-      } else {
-        return tags;
-      }
-    }
-
     if (id) {
       return { id: id, name: name, email: email, phone: phone, tags: tags} 
     } else {
       return { name: name, email: email, phone: phone, tags, tags } 
+    }
+  },
+
+  formVal: function(string, form) {
+    return form.find(`input[name="${string}"]`).val();
+  },
+
+  tagValues: function(form) {
+    let tagVals = form.find(`input[name="tags"]`).val();
+    let tags = tagVals.split(',').map(string => string.trim());
+    if (tags.length === 0 || (tags.length === 1 && tags[0] === '')) {
+      return [];
+    } else {
+      return tags;
     }
   },
 
@@ -213,7 +286,7 @@ let App = {
   },
 
   filterSearch: function(string) {
-    let contactsArray = this.retrieveContacts();
+    let contactsArray = Contacts.list;
     let filteredArray = [];
     let regexp = new RegExp(string, 'i');
     for (let i = 0; i < contactsArray.length; i++) {
